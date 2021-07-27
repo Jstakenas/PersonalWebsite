0b18d34216ceb8baa6f528cd4d4d435fc6ff3ac5
Jordan <jordan.stakenas@enosix.com>
2021-07-27 12:32:18 -0600

    further refinement placing and spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,8 @@ class Home extends Component {
                 <div  style={{height: 100+"%"}}>
                     <div>
                         <div className="header">
-                            jordan stakenas
+                            <div className="headerName">jordan stakenas</div>
+                            <div className="headerHome">home</div>
                         </div>
                         <div className="linebreak"/>
                         <div className="subheader">
@@ -19,16 +20,27 @@ class Home extends Component {
                         </div>
                     </div>
                     <div className="body">
-                        <h1 className="welcome">
-                        Welcome
-                        </h1>
+                        <div className="bodyS1">
+                            <h1 className="welcome">
+                                Welcome
+                            </h1>
 
-                        <br/>
-                        <div className="ProjList">
-                            <ul>
-                                <lic>this website is heavily WIP.</lic>
-                                <lic>If you got here.. how?</lic>
-                            </ul>
+                            <br/><br/>
+                            <div className="ProjList">
+                                <ul>
+                                    <lic>this website is heavily WIP.</lic>
+                                    <br/>
+                                    <lic>If you got here.. how?</lic>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="bodyS2">
+                            text
+                        </div>
+                        <div className="bodyS3">
+                            <div className="bodyS4">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis justo et sapien efficitur mollis. Cras scelerisque pellentesque tellus in lacinia. Vestibulum vehicula magna ac nisi dapibus dignissim. Curabitur ut volutpat libero, sed ultricies nisi. Praesent viverra ex id odio convallis, at aliquet dolor gravida. Duis vitae arcu non leo pretium dictum non in elit. Suspendisse nibh ante, facilisis venenatis libero sit amet, tristique facilisis nunc.
+                            </div>
                         </div>
                     </div>
                     <div className="watermark"><b>-__-</b></div>
